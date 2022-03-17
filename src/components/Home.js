09d8d7f06f68c5a10ab3c24aa46fc8ddcfbd47be
priyaljain03 <jainpriyal03@gmail.com>
2022-03-17 16:49:40 +0530

    Added Grow Transition to Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from 'react'
+import {
+    Link
+} from 'react-router-dom'
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import '../css/Home.css'
 import ColorChip from "./ColorChip";
+import Loader from "./Loader";
+import Grow from '@mui/material/Grow';
 
 function Home() {
     const [animeList, setAnimeList] = useState([]) //List for all fetched animes
+    const [checked,setChecked] = useState(false)
 
     const Item = styled(Paper)(({ theme }) => ({
         backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -19,6 +25,7 @@ function Home() {
 
     //Fetching Anime List from API URL and setting list as state variable animeList
     useEffect(async () => {
+        console.log(animeList)
         fetch('https://ghibliapi.herokuapp.com/films')
             .then(resp => {
                 console.log(resp)
@@ -26,37 +33,43 @@ function Home() {
             })
             .then(res => {
                 console.log(res)
+                setChecked(true)
                 setAnimeList(res)
             })
     }, [])
 
     //Returning Grid view for list of animes
     return (
-        <>
-            <Box sx={{ flexGrow: 1 }} style={{ padding: '70px' }}>
-                <Grid container columns={{ xs: 12, sm: 12, md: 18,lg:12 }} alignItems="center" columnSpacing={{ xs: 1, sm: 2, md: 3 }} rowSpacing={{ xs: 4, sm: 8, md: 15 }}>
-                    {animeList.map((anime) => {
-                        //Looping through list of animes
-                        return (
-                            <Grid item xs={12} sm={6} md={6} lg={2}>
-                                <div className="gridBox">
-                                    <div className="chip">
-                                    <ColorChip rt_score={anime.rt_score + "%"}/>
-                                    </div>
-                                    
-                                    <img src={anime.image} />
-                                    <div className="content">
-                                        <span>{anime.title} | {anime.original_title}</span>
-                                        <br />
-                                        <span>Release - {anime.release_date}</span>
-                                    </div>
-                                </div>
-                            </Grid>
-                        )
-                    })}
-                </Grid>
-            </Box>
-        </>
+        <div>
+            {animeList.length == 0 ? <Loader /> :
+                <Box sx={{ flexGrow: 1 }} style={{ padding: '70px' }}>
+
+                    <Grid container columns={{ xs: 12, sm: 12, md: 18, lg: 12 }} alignItems="center" columnSpacing={{ xs: 1, sm: 2, md: 3 }} rowSpacing={{ xs: 4, sm: 8, md: 9 }}>
+                        {animeList.map((anime) => {
+                            //Looping through list of animes
+                            return (
+                                <Grow direction="up" in={checked}  mountOnEnter unmountOnExit>
+                                    <Grid item xs={12} sm={6} md={6} lg={2}>
+                                        <div className="gridBox">
+                                            <div className="chip">
+                                                <ColorChip rt_score={anime.rt_score + "%"} />
+                                            </div>
+
+                                            <Link to={`/detail/${anime.id}`}><img src={anime.image} /></Link>
+                                            <div className="content">
+                                                <span>{anime.title} | {anime.original_title}</span>
+                                                <br />
+                                                <span>Release - {anime.release_date}</span>
+                                            </div>
+                                        </div>
+                                    </Grid>
+                                </Grow>
+                            )
+                        })}
+                    </Grid>
+                </Box>
+            }
+        </div>
     )
 }
 
