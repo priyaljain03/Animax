ce91235d108d0a4bd5bcb9e2200bc6fb3b23dc34
priyaljain03 <jainpriyal03@gmail.com>
2022-03-18 13:49:12 +0530

    Changed Navbar and Used MUI component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,182 @@
-import React from 'react';
+import * as React from 'react';
 import Logo from './Logo';
-import '../css/Navbar.css';
-import CasinoIcon from '@mui/icons-material/Casino';
+import '../css/Navbar.css'
 import Casino from '@mui/icons-material/Casino';
 import SearchIcon from '@mui/icons-material/Search';
-function Navbar() {
-    return (
-        <div className="nav">
-            <div className="logo-wrapper">
-                <Logo />
-            </div>
-            <ul className="menu-list">
-                <li><button className="premium-button">Try Premium</button></li>
-                <li ><Casino style={{padding:'0px'}}/>Random</li>
-                <li><SearchIcon /> Search</li>
-                <li><button className="login-button">Login</button></li>
-            </ul>
-        </div>
-    )
-}
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import FaceOutlinedIcon from '@mui/icons-material/FaceOutlined';
 
-export default Navbar
+//Created Dark Theme for NavBar
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#1976d2',
+        },
+    },
+});
+
+
+// Responsive Navbar JSX starts here
+const ResponsiveAppBar = () => {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <AppBar position="static">
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        {/* Mobile Menu View */}
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="div"
+                            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+                        >
+                            <Logo />
+                        </Typography>
+
+                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            {/* Mobile menu Icon */}
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            {/* MobileMenu starts Here */}
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: { xs: 'block', md: 'none' },
+                                }}
+                            >
+                                <MenuItem key="random" onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center"><FaceOutlinedIcon /> LOGIN</Typography>
+                                </MenuItem>
+                                <MenuItem key="login" onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center"><Casino className="icon" /> RANDOM</Typography>
+                                </MenuItem>
+                                <MenuItem key="search" onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center"><SearchIcon className="icon" /> SEARCH</Typography>
+                                </MenuItem>
+                            </Menu>
+                        </Box>
+
+                        {/* Bigger screens view  */}
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="div"
+                            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+                        >
+                            <Logo />
+                        </Typography>
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        </Box>
+
+                        {/* Menu Items starts here */}
+                        <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
+                            <Button sx={{
+                                my: 2, color: 'black', fontWeight: '1000', fontSize: '10px',
+                                display: 'block', backgroundColor: 'orange',
+                                '&:hover': {
+                                    backgroundColor: 'rgb(233, 175, 68)',
+                                },
+                            }}>Try Premium</Button>
+                            <MenuItem
+                                key={1}
+                                onClick={handleCloseNavMenu}
+                                sx={{
+                                    my: 2, color: 'white',
+                                    display: 'block',
+                                    '&:hover': {
+                                        color: 'orangered',
+                                        transition: 'color 300ms linear',
+                                        background: 'none'
+                                    }
+                                }}
+                            >
+                                <Casino className="icon" />Random
+                            </MenuItem>
+                            <MenuItem
+                                key={1}
+                                onClick={handleCloseNavMenu}
+                                sx={{
+                                    my: 2, color: 'white', display: 'block',
+                                    '&:hover': {
+                                        color: 'orangered',
+                                        transition: 'color 300ms linear',
+                                        background: 'none'
+                                    }
+                                }}
+                            >
+                                <SearchIcon className="icon" />Search
+                            </MenuItem>
+
+                            <Button
+                                key={1}
+                                onClick={handleCloseNavMenu}
+                                sx={{
+                                    my: 2, color: 'white', display: 'block',
+                                    backgroundColor: 'transparent',
+                                    border: '1px solid white',
+                                    '&:hover': {
+                                        backgroundColor: 'white',
+                                        color: 'black'
+                                    },
+                                }}
+                            >
+                                LogIn
+                            </Button>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </ThemeProvider>
+    );
+};
+
+export default ResponsiveAppBar
